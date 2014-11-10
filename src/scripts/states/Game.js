74e73783ecd074d6b7d40092b595f11174c51f35
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2014-11-10 17:00:20 -0200

    Moving things.

--- src/scripts/states/Game.js
+++ src/scripts/states/Game.js
@@ -5,7 +5,7 @@ class Game extends Phaser.State {
 
     this.stageName = stageName;
 
-    this._objectsManager = new ObjectsManager(this.game);
+    this._objectsManager = this._makeManager(ObjectsManager);
 
     this._playerActor = null;
     this._idleActor   = null;
@@ -16,10 +16,7 @@ class Game extends Phaser.State {
 
     var { heartLayer, starLayer } = stages.getRelatedLayerNames(this.stageName);
 
-    var mapObjects = this._getTilemapObjects('tilemaps', this.stageName);
-    this.heartCoordinates = mapObjects['actors']['heart'];
-    this.starCoordinates  = mapObjects['actors']['star'];
-    this.goalCoordinates  = mapObjects['actors']['goal'];
+    this._mapObjects = this._getTilemapObjects('tilemaps', this.stageName);
 
     this._agents = this.add.existing(new Agents(this.game));
     this._agents.actorFellOff.add(this._fellOff, this);
@@ -34,7 +31,7 @@ class Game extends Phaser.State {
     this._tilemap2 = this._makeTilemap(starLayer);
     this._layer2 = this._starGroup.add(this._makeTilemapLayer(this._tilemap2, starLayer));
 
-    this._objectsManager.createObjects(mapObjects);
+    this._objectsManager.createObjects(this._mapObjects);
 
     this._goal = this.add.existing(
       new Goal(
@@ -43,20 +40,11 @@ class Game extends Phaser.State {
         this.goalCoordinates.y));
     this._goal.actorsLanded.addOnce(this._goToNextStage, this);
 
-    this._heart = this.add.existing(
-      new Actor(
-        this.game,
-        this.heartCoordinates.x,
-        this.heartCoordinates.y,
-        Actor.HEART));
-    this._star = this.add.existing(
-      new Actor(
-        this.game,
-        this.starCoordinates.x,
-        this.starCoordinates.y,
-        Actor.STAR));
+    this._heart = this.add.existing(this._makeActor(Actor.HEART));
+    this._star  = this.add.existing(this._makeActor(Actor.STAR));
 
-    this._setupPlayableActors(this._heart, this._star);
+    this._restartActors();
+    this._changeActors(this._heart, this._star);
 
     this.controls.spacebar.onUp.add(this._togglePlayerActor, this);
     this.controls.backspace.onUp.add(this._restartActors, this);
@@ -97,6 +85,10 @@ class Game extends Phaser.State {
 
   // --------------------------------------------------------------------------
 
+  _makeManager (factory, ... args) {
+    return new factory(this.game, ... args);
+  }
+
   _getTilemapObjects (key, stageName) {
     var parsedTilemap = Phaser.TilemapParser.parse(this.game, key);
     var stageObjects  = parsedTilemap.objects[stageName];
@@ -117,7 +109,11 @@ class Game extends Phaser.State {
     return tilemap.createLayer(layerName);
   }
 
-  _setupPlayableActors(playerActor, idleActor) {
+  _makeActor (roleName) {
+    return new Actor(this.game, 0, 0, roleName);
+  }
+
+  _changeActors(playerActor, idleActor) {
     this._playerActor      = playerActor;
     this._playerActor.idle = false;
 
@@ -134,7 +130,7 @@ class Game extends Phaser.State {
   _togglePlayerActor () {
     if (!this._playerActor.standing) return;
 
-    this._setupPlayableActors(this._idleActor, this._playerActor);
+    this._changeActors(this._idleActor, this._playerActor);
 
     this._playerActor.stop();
     this._idleActor.stop();
@@ -156,6 +152,20 @@ class Game extends Phaser.State {
       this._restartActors();
     else
       this.state.start('Game', true, false, nextStage);
+  }
+
+  // --------------------------------------------------------------------------
+
+  get heartCoordinates () {
+    return this._mapObjects.actors.heart;
+  }
+
+  get starCoordinates () {
+    return this._mapObjects.actors.star;
+  }
+
+  get goalCoordinates () {
+    return this._mapObjects.actors.goal;
   }
 
 }
