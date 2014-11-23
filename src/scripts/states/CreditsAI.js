275784455b80e939bcac28050c74c6ee54b0e721
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2014-11-23 16:41:55 -0200

    Adding the urls to the buttons.

--- src/scripts/states/CreditsAI.js
+++ src/scripts/states/CreditsAI.js
@@ -15,13 +15,26 @@ class CreditsAI extends Phaser.State {
     this.add.existing(new BackButton(this.game));
     this.add.existing(new MoreButton(this.game));
 
-    this.add.existing(new LinkButton(this.game,  64, LinkButton.FACEBOOK));
-    this.add.existing(new LinkButton(this.game,  96, LinkButton.TWITTER));
-    this.add.existing(new LinkButton(this.game, 128, LinkButton.DEVIANTART));
-    this.add.existing(new LinkButton(this.game, 160, LinkButton.ADVENTURE_ISLANDS));
+    var links = [
+      [  64, LinkButton.FACEBOOK,          'https://www.facebook.com/AdventureIslandsGames' ],
+      [  96, LinkButton.TWITTER,           'https://twitter.com/AdventIslands'              ],
+      [ 128, LinkButton.DEVIANTART,        'http://adventureislands.deviantart.com/'        ],
+      [ 160, LinkButton.ADVENTURE_ISLANDS, 'http://simpanen.carbonmade.com/'                ]
+    ];
+
+    this._addLinkButtons(links);
   }
 
   // --------------------------------------------------------------------------
+
+  _makeLinkButton([ x, faceTexture, url ]) {
+    return new LinkButton(this.game, x, faceTexture, url);
+  }
+
+  _addLinkButtons (links) {
+    for (var link of links)
+      this.add.existing(this._makeLinkButton(link));
+  }
 
 }
 
--- src/scripts/states/CreditsRB.js
+++ src/scripts/states/CreditsRB.js
@@ -14,11 +14,25 @@ class Credits extends Phaser.State {
 
     this.add.existing(new BackButton(this.game));
 
-    this.add.existing(new LinkButton(this.game,  96, LinkButton.TWITTER));
-    this.add.existing(new LinkButton(this.game, 128, LinkButton.DEVIANTART));
+    var links = [
+      [  96, LinkButton.TWITTER, 'https://twitter.com/rb_lopes' ],
+      [ 128, LinkButton.GITHUB,  'https://github.com/rblopes'   ]
+    ];
+
+    this._addLinkButtons(links);
   }
 
   // --------------------------------------------------------------------------
+
+  _makeLinkButton([ x, faceTexture, url ]) {
+    return new LinkButton(this.game, x, faceTexture, url);
+  }
+
+  _addLinkButtons (links) {
+    for (var link of links)
+      this.add.existing(this._makeLinkButton(link));
+  }
+
 
 }
 
