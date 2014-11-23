3ed4f820eee5d0aeba5bc8a0667784f805b83b71
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2014-11-23 18:09:02 -0200

    Adding plugin `gulp-cached`, no long linting times anymore.

--- gulp/tasks/jshint.js
+++ gulp/tasks/jshint.js
@@ -1,4 +1,5 @@
 var gulp         = require('gulp');
+var cache        = require('gulp-cached');
 var jshint       = require('gulp-jshint');
 var reload       = require('browser-sync').reload;
 var handleErrors = require('../util/handleErrors');
@@ -9,6 +10,7 @@ var paths = projectConfig.paths;
 gulp.task('jshint', function () {
   return gulp.src([ paths['scripts'] ])
     .pipe(handleErrors())
+    .pipe(cache('jshint'))
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(reload({ stream: true }));
