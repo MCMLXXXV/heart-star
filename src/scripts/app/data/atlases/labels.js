86434ba16749095232662f7dc4d324a31178efc5
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-03-13 13:05:08 -0300

    Labels texture atlas must have a padding

--- src/scripts/app/data/atlases/labels.js
+++ src/scripts/app/data/atlases/labels.js
@@ -6,52 +6,52 @@ export default {
   },
   frames: {
     'label-version': {
-      frame: { x: 480, y: 218, w: 34, h: 8 },
+      frame: { x: 490, y: 344, w: 34, h: 8 },
       spriteSourceSize: { x: 0, y: 0, w: 34, h: 8 },
       sourceSize: { w: 34, h: 8 }
     },
     'label-level-select': {
-      frame: { x: 480, y: 208, w: 240, h: 10 },
+      frame: { x: 490, y: 330, w: 240, h: 10 },
       spriteSourceSize: { x: 0, y: 0, w: 240, h: 10 },
       sourceSize: { w: 240, h: 10 }
     },
     'label-title': {
-      frame: { x: 480, y: 160, w: 240, h: 48 },
+      frame: { x: 246, y: 330, w: 240, h: 48 },
       spriteSourceSize: { x: 0, y: 0, w: 240, h: 48 },
       sourceSize: { w: 240, h: 48 }
     },
     'logo-rb': {
-      frame: { x: 480, y: 0, w: 240, h: 160 },
+      frame: { x: 2, y: 330, w: 240, h: 160 },
       spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
       sourceSize: { w: 240, h: 160 }
     },
     'logo-adventure-islands': {
-      frame: { x: 240, y: 320, w: 240, h: 160 },
+      frame: { x: 490, y: 166, w: 240, h: 160 },
       spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
       sourceSize: { w: 240, h: 160 }
     },
     'label-hero-blocks': {
-      frame: { x: 0, y: 320, w: 240, h: 160 },
+      frame: { x: 490, y: 2, w: 240, h: 160 },
       spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
       sourceSize: { w: 240, h: 160 }
     },
     'label-game-primer': {
-      frame: { x: 240, y: 160, w: 240, h: 160 },
+      frame: { x: 246, y: 166, w: 240, h: 160 },
       spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
       sourceSize: { w: 240, h: 160 }
     },
     'label-credits-rb': {
-      frame: { x: 0, y: 160, w: 240, h: 160 },
+      frame: { x: 2, y: 166, w: 240, h: 160 },
       spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
       sourceSize: { w: 240, h: 160 }
     },
     'label-credits-ai': {
-      frame: { x: 240, y: 0, w: 240, h: 160 },
+      frame: { x: 246, y: 2, w: 240, h: 160 },
       spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
       sourceSize: { w: 240, h: 160 }
     },
     'label-carrying-partners': {
-      frame: { x: 0, y: 0, w: 240, h: 160 },
+      frame: { x: 2, y: 2, w: 240, h: 160 },
       spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
       sourceSize: { w: 240, h: 160 }
     }
