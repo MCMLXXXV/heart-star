e01c728b7caac0674d964d56a82fa0823f0b6d23
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-03-15 15:11:13 -0300

    Updating recent texture atlases

--- src/scripts/app/data/atlases/labels.js
+++ src/scripts/app/data/atlases/labels.js
@@ -1,54 +1,54 @@
 export default {
   frames: {
-    'label-version': {
-      frame: { x: 490, y: 344, w: 34, h: 8 },
-      spriteSourceSize: { x: 0, y: 0, w: 34, h: 8 },
-      sourceSize: { w: 34, h: 8 }
-    },
-    'label-level-select': {
-      frame: { x: 490, y: 330, w: 240, h: 10 },
-      spriteSourceSize: { x: 0, y: 0, w: 240, h: 10 },
-      sourceSize: { w: 240, h: 10 }
-    },
-    'label-title': {
-      frame: { x: 246, y: 330, w: 240, h: 48 },
-      spriteSourceSize: { x: 0, y: 0, w: 240, h: 48 },
-      sourceSize: { w: 240, h: 48 }
-    },
-    'logo-rb': {
-      frame: { x: 2, y: 330, w: 240, h: 160 },
-      spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
-      sourceSize: { w: 240, h: 160 }
-    },
-    'logo-adventure-islands': {
-      frame: { x: 490, y: 166, w: 240, h: 160 },
-      spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
-      sourceSize: { w: 240, h: 160 }
-    },
-    'label-hero-blocks': {
-      frame: { x: 490, y: 2, w: 240, h: 160 },
-      spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
-      sourceSize: { w: 240, h: 160 }
-    },
-    'label-game-primer': {
-      frame: { x: 246, y: 166, w: 240, h: 160 },
-      spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
-      sourceSize: { w: 240, h: 160 }
-    },
-    'label-credits-rb': {
-      frame: { x: 2, y: 166, w: 240, h: 160 },
+    'label-carrying-partners': {
+      frame: { x: 0, y: 0, w: 240, h: 160 },
       spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
       sourceSize: { w: 240, h: 160 }
     },
     'label-credits-ai': {
-      frame: { x: 246, y: 2, w: 240, h: 160 },
+      frame: { x: 242, y: 0, w: 240, h: 160 },
       spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
       sourceSize: { w: 240, h: 160 }
     },
-    'label-carrying-partners': {
-      frame: { x: 2, y: 2, w: 240, h: 160 },
+    'label-credits-rb': {
+      frame: { x: 0, y: 162, w: 240, h: 160 },
       spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
       sourceSize: { w: 240, h: 160 }
-    }
+    },
+    'label-game-primer': {
+      frame: { x: 242, y: 162, w: 240, h: 160 },
+      spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
+      sourceSize: { w: 240, h: 160 }
+    },
+    'label-hero-blocks': {
+      frame: { x: 484, y: 0, w: 240, h: 160 },
+      spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
+      sourceSize: { w: 240, h: 160 }
+    },
+    'label-level-select': {
+      frame: { x: 484, y: 324, w: 240, h: 10 },
+      spriteSourceSize: { x: 0, y: 0, w: 240, h: 10 },
+      sourceSize: { w: 240, h: 10 }
+    },
+    'label-title': {
+      frame: { x: 242, y: 324, w: 240, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 240, h: 48 },
+      sourceSize: { w: 240, h: 48 }
+    },
+    'label-version': {
+      frame: { x: 484, y: 336, w: 34, h: 8 },
+      spriteSourceSize: { x: 0, y: 0, w: 34, h: 8 },
+      sourceSize: { w: 34, h: 8 }
+    },
+    'logo-adventure-islands': {
+      frame: { x: 484, y: 162, w: 240, h: 160 },
+      spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
+      sourceSize: { w: 240, h: 160 }
+    },
+    'logo-rb': {
+      frame: { x: 0, y: 324, w: 240, h: 160 },
+      spriteSourceSize: { x: 0, y: 0, w: 240, h: 160 },
+      sourceSize: { w: 240, h: 160 }
+    },
   }
 };
