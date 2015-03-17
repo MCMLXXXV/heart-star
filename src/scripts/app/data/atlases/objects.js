2396df195f3f73f66a098a1c3920a8a3df5b184f
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-03-17 14:20:42 -0300

    Texture atlas of the game objects

---
+++ src/scripts/app/data/atlases/objects.js
@@ -0,0 +1,184 @@
+export default {
+  frames: {
+    'goal-1': {
+      frame: { x: 0, y: 0, w: 32, h: 32 },
+      spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 },
+      sourceSize: { w: 32, h: 32 }
+    },
+    'goal-2': {
+      frame: { x: 68, y: 34, w: 32, h: 32 },
+      spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 },
+      sourceSize: { w: 32, h: 32 }
+    },
+    'goal-3': {
+      frame: { x: 68, y: 0, w: 32, h: 32 },
+      spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 },
+      sourceSize: { w: 32, h: 32 }
+    },
+    'goal-4': {
+      frame: { x: 34, y: 0, w: 32, h: 32 },
+      spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 },
+      sourceSize: { w: 32, h: 32 }
+    },
+    'goal-5': {
+      frame: { x: 0, y: 34, w: 32, h: 32 },
+      spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 },
+      sourceSize: { w: 32, h: 32 }
+    },
+    'goal-6': {
+      frame: { x: 34, y: 34, w: 32, h: 32 },
+      spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 },
+      sourceSize: { w: 32, h: 32 }
+    },
+    'heart-button-1': {
+      frame: { x: 108, y: 136, w: 16, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 16 },
+      sourceSize: { w: 16, h: 16 }
+    },
+    'heart-button-2': {
+      frame: { x: 90, y: 136, w: 16, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 16 },
+      sourceSize: { w: 16, h: 16 }
+    },
+    'heart-platform-fixed-normal': {
+      frame: { x: 70, y: 118, w: 32, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 32, h: 16 },
+      sourceSize: { w: 32, h: 16 }
+    },
+    'heart-platform-fixed-small': {
+      frame: { x: 72, y: 136, w: 16, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 16 },
+      sourceSize: { w: 16, h: 16 }
+    },
+    'heart-retractable-1': {
+      frame: { x: 18, y: 68, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'heart-retractable-2': {
+      frame: { x: 36, y: 68, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'heart-retractable-3': {
+      frame: { x: 18, y: 118, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'heart-retractable-4': {
+      frame: { x: 72, y: 68, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'heart-retractable-5': {
+      frame: { x: 90, y: 68, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'heart-trap': {
+      frame: { x: 54, y: 136, w: 16, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 16 },
+      sourceSize: { w: 16, h: 16 }
+    },
+    'moon-button-1': {
+      frame: { x: 156, y: 100, w: 16, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 16 },
+      sourceSize: { w: 16, h: 16 }
+    },
+    'moon-button-2': {
+      frame: { x: 138, y: 118, w: 16, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 16 },
+      sourceSize: { w: 16, h: 16 }
+    },
+    'moon-platform-fixed-normal': {
+      frame: { x: 36, y: 118, w: 32, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 32, h: 16 },
+      sourceSize: { w: 32, h: 16 }
+    },
+    'moon-platform-fixed-small': {
+      frame: { x: 138, y: 100, w: 16, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 16 },
+      sourceSize: { w: 16, h: 16 }
+    },
+    'moon-retractable-1': {
+      frame: { x: 156, y: 0, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'moon-retractable-2': {
+      frame: { x: 156, y: 50, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'moon-retractable-3': {
+      frame: { x: 0, y: 118, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'moon-retractable-4': {
+      frame: { x: 54, y: 68, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'moon-retractable-5': {
+      frame: { x: 138, y: 50, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'moon-trap': {
+      frame: { x: 36, y: 136, w: 16, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 16 },
+      sourceSize: { w: 16, h: 16 }
+    },
+    'star-button-1': {
+      frame: { x: 120, y: 100, w: 16, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 16 },
+      sourceSize: { w: 16, h: 16 }
+    },
+    'star-button-2': {
+      frame: { x: 102, y: 50, w: 16, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 16 },
+      sourceSize: { w: 16, h: 16 }
+    },
+    'star-platform-fixed-normal': {
+      frame: { x: 104, y: 118, w: 32, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 32, h: 16 },
+      sourceSize: { w: 32, h: 16 }
+    },
+    'star-platform-fixed-small': {
+      frame: { x: 156, y: 118, w: 16, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 16 },
+      sourceSize: { w: 16, h: 16 }
+    },
+    'star-retractable-1': {
+      frame: { x: 120, y: 50, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'star-retractable-2': {
+      frame: { x: 120, y: 0, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'star-retractable-3': {
+      frame: { x: 0, y: 68, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'star-retractable-4': {
+      frame: { x: 102, y: 0, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'star-retractable-5': {
+      frame: { x: 138, y: 0, w: 16, h: 48 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 48 },
+      sourceSize: { w: 16, h: 48 }
+    },
+    'star-trap': {
+      frame: { x: 126, y: 136, w: 16, h: 16 },
+      spriteSourceSize: { x: 0, y: 0, w: 16, h: 16 },
+      sourceSize: { w: 16, h: 16 }
+    },
+  }
+};
