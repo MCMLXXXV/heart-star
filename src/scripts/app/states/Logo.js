598fe2bdf8fd43cc53267598888a557af70d06d5
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-03-18 15:06:24 -0300

    Simplify 'Logo' state to its bare minimun
    Could be more complicated. But `Tween#from` is surely broken, I couldn't keep
    those 'bug' lines there any longer.

--- src/scripts/app/states/Logo.js
+++ src/scripts/app/states/Logo.js
@@ -1,49 +1,24 @@
 export default class Logo extends Phaser.State {
 
   create () {
-    this.stage.backgroundColor = 0x000000; // black
+    let logo = this.add.image(0, 0, 'labels', 'logo-adventure-islands');
+    logo.alpha = 0;
 
-    var aiLogo = this.add.image(0, 0, 'labels', 'logo-adventure-islands');
-    var rbLogo = this.add.image(0, 0, 'labels', 'logo-rb');
-    rbLogo.alpha = 0; // Phaser BUG
+    const changeLogo = () => logo.frameName = 'logo-rb';
+    const goToTitle  = () => this.state.start('Title', true, false, 'iris');
+    const makeTween  = (callback) => {
+      let tween = this.add.tween(logo)
+        .to({ alpha: 1 }, 1000, 'Sine.easeOut', false, 1500)
+        .to({ alpha: 0 }, 1000, 'Sine.easeOut', false, 1500);
 
-    var [ first, second ] = this._makeLogoFadeEffect(aiLogo);
-    var [      , last   ] = this._makeLogoFadeEffect(rbLogo, second);
+      tween.onComplete.addOnce(callback);
 
-    first.start();
-    second.onComplete.addOnce(() => rbLogo.alpha = 1, this); // Phaser BUG
-    last.onComplete.addOnce(this._goToNextState, this);
-  }
+      return tween;
+    };
 
-  // --------------------------------------------------------------------------
-
-  _makeTween (object, chainWith = null) {
-    var tween = this.add.tween(object);
-
-    if (chainWith !== null)
-      chainWith.chain(tween);
-
-    return tween;
-  }
-
-  _makeLogoFadeEffect (logo, chainWith = null) {
-    var reveal = this._makeTween(logo, chainWith);
-    var fade   = this._makeTween(logo, reveal);
-
-    reveal.from({ alpha: 0 }, 1000, 'Sine.easeOut', false, 1500);
-    fade.to({ alpha: 0 }, 1000, 'Sine.easeOut', false, 1500);
-
-    return [ reveal, fade ];
-  }
-
-  _goToNextState () {
-    this.state.start(this._nextState, true, false, 'iris');
-  }
-
-  // --------------------------------------------------------------------------
-
-  get _nextState () {
-    return 'Title';
+    makeTween(changeLogo)
+      .chain(makeTween(goToTitle))
+      .start();
   }
 
 }
