7bc9bc22731ae7086aee9c82066741c6c6050f69
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-03-19 13:00:09 -0300

    A `states` module

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,23 +1,12 @@
-import Boot    from './app/states/Boot';
-import Preload from './app/states/Preload';
-import Logo    from './app/states/Logo';
-import Title   from './app/states/Title';
-import Levels  from './app/states/Levels';
-import Credits from './app/states/Credits';
-import Game    from './app/states/Game';
+import * as states from './app/states';
+
 
 export function start () {
   var game = new Phaser.Game(240, 160, Phaser.AUTO);
 
-  [
-    [ 'Boot'   , Boot    ],
-    [ 'Preload', Preload ],
-    [ 'Logo'   , Logo    ],
-    [ 'Title'  , Title   ],
-    [ 'Levels' , Levels  ],
-    [ 'Credits', Credits ],
-    [ 'Game'   , Game    ],
-  ].map(([name, state]) => game.state.add(name, state));
+  Object.keys(states)
+    .map((key) => [ key, states[key] ])
+    .forEach((args) => game.state.add(... args));
 
   game.state.start('Boot');
 
---
+++ src/scripts/app/states.js
@@ -0,0 +1,7 @@
+export { default as Boot    } from './states/Boot';
+export { default as Preload } from './states/Preload';
+export { default as Logo    } from './states/Logo';
+export { default as Title   } from './states/Title';
+export { default as Levels  } from './states/Levels';
+export { default as Credits } from './states/Credits';
+export { default as Game    } from './states/Game';
