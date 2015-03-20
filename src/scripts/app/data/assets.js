fb4c3e04d5d85c46fc3beee6500512b475f363fb
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-03-20 13:34:59 -0300

    Flattening the 'assets' directory

--- src/scripts/app/data/assets.js
+++ src/scripts/app/data/assets.js
@@ -11,7 +11,7 @@ export default {
     {
       'type': 'image',
       'key': 'splash-screen',
-      'url': 'images/splash-screen.png'
+      'url': 'splash-screen.png'
     }
 
   ],
@@ -23,27 +23,27 @@ export default {
     {
       'type': 'atlasJSONHash',
       'key': 'graphics',
-      'textureURL': 'images/graphics.png',
+      'textureURL': 'graphics.png',
       'atlasData': graphics
     },
 
     {
       'type': 'atlasJSONHash',
       'key': 'sprites',
-      'textureURL': 'images/sprites.png',
+      'textureURL': 'sprites.png',
       'atlasData': sprites
     },
 
     {
       'type': 'image',
       'key': 'tileset',
-      'url': 'images/tileset.png',
+      'url': 'tileset.png',
     },
 
     {
       'type': 'tilemap',
       'key': 'tilemaps',
-      'url': 'tilemaps/tilemaps.json',
+      'url': 'tilemaps.json',
       'format': 'TILED_JSON'
     }
 
