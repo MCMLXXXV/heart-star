efac1ad426b78c633d73b4ebfd97a497448581c4
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-08-05 15:23:06 -0300

    Updating tooling to `my-phaser-template@0.4.1` stuff.

--- gulpfile.js/config/pluginOptions.js
+++ gulpfile.js/config/pluginOptions.js
@@ -1,23 +1,34 @@
+/*
+ * Task Plugin Options
+ * ============================================================================
+ */
+
+'use strict';
+
+
 module.exports = {
+
   'dist:styles': {
-    'keepSpecialComments': false,
-    'removeEmpty': true
+    keepSpecialComments: false,
+    removeEmpty: true
   },
+
   'dist:appcache': {
-    'filename': 'offline.appcache',
-    'cache': [
+    filename: 'offline.appcache',
+    cache: [
       'game.min.js',
       'styles.min.css'
     ],
-    'preferOnline': true,
-    'timestamp': true,
-    'network': [
+    preferOnline: true,
+    timestamp: true,
+    network: [
       'https://*',
       'http://*',
       '*'
     ],
-    'fallback': [
+    fallback: [
       '. index.html'
     ]
   }
+
 };
--- gulpfile.js/tasks/tasks.js
+++ gulpfile.js/tasks/tasks.js
@@ -1,12 +1,20 @@
+/*
+ * Internal development supporting tasks.
+ * ============================================================================
+ */
+
 'use strict';
 
 
 module.exports = function (gulp, $) {
 
   gulp.task('tasks:lint', function () {
-    gulp.src('./gulpfile.js/**/*.js')
-      .pipe($.jshint('.jshintrc.tasks'))
-      .pipe($.jshint.reporter('jshint-stylish'));
+    gulp.src('gulpfile.js/**/*.js')
+      .pipe($.eslint({
+        useEslintrc: false,
+        configFile: 'gulpfile.js/.eslintrc'
+      }))
+      .pipe($.eslint.format('stylish', process.stderr));
   });
 
 };
