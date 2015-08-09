cfe3fdbffe89cd64f47a35f87a03efe0d098b0d5
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-08-08 23:46:09 -0300

    Just create the sprites, then resize their physics bodies.

--- src/scripts/app/objects/Agents.js
+++ src/scripts/app/objects/Agents.js
@@ -5,9 +5,9 @@ class Agents extends Phaser.Group {
 
     this.enableBody = true;
 
-    this._leftAgent   = this.add(this._makeAgent( -20,   0,  16, 160));
-    this._rightAgent  = this.add(this._makeAgent( 244,   0,  16, 160));
-    this._bottomAgent = this.add(this._makeAgent(-120, 240, 480,  16));
+    this._leftAgent   = this._makeAgent( -20,   0,  16, 160);
+    this._rightAgent  = this._makeAgent( 244,   0,  16, 160);
+    this._bottomAgent = this._makeAgent(-120, 240, 480,  16);
 
     this.setAll('body.immovable', true);
   }
@@ -28,12 +28,11 @@ class Agents extends Phaser.Group {
 
   // --------------------------------------------------------------------------
 
-  _makeBitmap (width, height) {
-    return this.game.make.bitmapData(width, height);
-  }
-
   _makeAgent (x, y, width, height) {
-    return this.game.make.sprite(x, y, this._makeBitmap(width, height));
+    const sprite = this.create(x, y, null);
+    sprite.body.setSize(width, height);
+
+    return sprite;
   }
 
   _bottomAgentOverlapCallback (actor) {
