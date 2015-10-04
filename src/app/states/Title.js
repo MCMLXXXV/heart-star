98d8dd2daf1b19b134a0c37deed0a3959469a4d6
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-10-04 18:10:27 -0300

    Downgraded `Title` state class to an object.

--- src/app/states/Title.js
+++ src/app/states/Title.js
@@ -2,46 +2,44 @@ import scrollingPattern from '../components/scrollingPattern';
 import { menuButton } from '../components/uiButtons';
 
 
-export default class Title extends Phaser.State {
+function showMenuButtons (g) {
+  const addButton = (y) => g.add.button(80, y, 'graphics');
+
+  menuButton(addButton(110), 'start', 'Levels');
+  menuButton(addButton(130), 'credits', 'Credits');
+}
+
+
+export default {
 
   init (effectName = 'iris') {
     this.game.transitions.reveal(
-      effectName, 1000, this._makeMenuButtons, this);
-  }
+      effectName, 1000, () => showMenuButtons(this.game));
+  },
 
   create () {
+    const image = (x, y, k, s) => this.add.image(x, y, k, s);
+
+    const anchorXY = (x, y, o) => (o.anchor.set(x, y), o);
+    const addAnimation0 = (m, n, x, o) => (o.animations.add(0, m, n, x), o);
+
+    const frames = (s) => Phaser.Animation.generateFrameNames(
+      `actor-${s}-`, 4, 7, '', 2);
+    const placeCharacter = (s, x) => addAnimation0(
+      frames(s), 4, true, anchorXY(0.5, 1, image(x, 96, 'sprites'))).play(0);
+
     scrollingPattern(this.game);
-    this.add.image(0, 0, 'graphics', 'background-title');
-    this.add.image(this.world.width, 0, 'graphics', 'version')
-      .anchor.set(1, 0);
-    this._addSwingingTitleLabel(18);
-    this._placeCharacter('heart',  64);
-    this._placeCharacter('star' , 176);
-  }
+    image(0, 0, 'graphics', 'background-title');
+    anchorXY(1, 0, image(240, 0, 'graphics', 'version'));
 
-  // --------------------------------------------------------------------------
-
-  _makeMenuButtons () {
-    const addButton = (y) => this.add.button(80, y, 'graphics');
-
-    menuButton(addButton(110), 'start', 'Levels');
-    menuButton(addButton(130), 'credits', 'Credits');
-  }
-
-  _placeCharacter (name, x) {
-    const frames = Phaser.Animation.generateFrameNames(`actor-${name}-`, 4, 7, '', 2);
-    const sprite = this.add.sprite(x, 96, 'sprites');
-
-    sprite.anchor.set(0.5, 1);
-    sprite.animations.add('main', frames, 4, true).play();
-  }
-
-  _addSwingingTitleLabel (y) {
-    this.add.tween(this.add.image(0, y, 'graphics', 'title'))
+    this.add.tween(image(0, 18, 'graphics', 'title'))
       .to({ y: '+8' }, 1500, 'Quad.easeInOut')
       .to({ y: '-8' }, 1500, 'Quad.easeInOut')
       .loop()
       .start();
+
+    placeCharacter('heart',  64);
+    placeCharacter('star',  176);
   }
 
-}
+};
