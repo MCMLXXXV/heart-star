6a7fa9671afea75119e7fcee68445e530c1d0a23
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-10-19 13:14:41 -0200

    Updated spikes to have dynamic width.

--- src/app/data/atlases/sprites.js
+++ src/app/data/atlases/sprites.js
@@ -2,395 +2,395 @@ export default {
   frames: [
     {
       filename: 'actor-heart-00',
-      frame: { x: 0, y: 220, w: 24, h: 24 }
+      frame: { x: 78, y: 240, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-01',
-      frame: { x: 78, y: 194, w: 24, h: 24 }
+      frame: { x: 208, y: 214, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-02',
-      frame: { x: 26, y: 220, w: 24, h: 24 }
+      frame: { x: 104, y: 240, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-03',
-      frame: { x: 52, y: 220, w: 24, h: 24 }
+      frame: { x: 130, y: 240, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-04',
-      frame: { x: 78, y: 220, w: 24, h: 24 }
+      frame: { x: 156, y: 240, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-05',
-      frame: { x: 104, y: 220, w: 24, h: 24 }
+      frame: { x: 182, y: 240, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-06',
-      frame: { x: 130, y: 220, w: 24, h: 24 }
+      frame: { x: 208, y: 240, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-07',
-      frame: { x: 156, y: 220, w: 24, h: 24 }
+      frame: { x: 234, y: 240, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-08',
-      frame: { x: 182, y: 220, w: 24, h: 24 }
+      frame: { x: 268, y: 0, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-09',
-      frame: { x: 208, y: 220, w: 24, h: 24 }
+      frame: { x: 268, y: 26, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-10',
-      frame: { x: 252, y: 0, w: 24, h: 24 }
+      frame: { x: 268, y: 52, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-11',
-      frame: { x: 252, y: 26, w: 24, h: 24 }
+      frame: { x: 268, y: 78, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-12',
-      frame: { x: 252, y: 52, w: 24, h: 24 }
+      frame: { x: 268, y: 104, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-13',
-      frame: { x: 252, y: 78, w: 24, h: 24 }
+      frame: { x: 268, y: 130, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-14',
-      frame: { x: 252, y: 104, w: 24, h: 24 }
+      frame: { x: 268, y: 156, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-15',
-      frame: { x: 252, y: 130, w: 24, h: 24 }
+      frame: { x: 268, y: 182, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-16',
-      frame: { x: 252, y: 156, w: 24, h: 24 }
+      frame: { x: 268, y: 208, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-17',
-      frame: { x: 252, y: 182, w: 24, h: 24 }
+      frame: { x: 268, y: 234, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-18',
-      frame: { x: 252, y: 208, w: 24, h: 24 }
+      frame: { x: 0, y: 266, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-19',
-      frame: { x: 0, y: 246, w: 24, h: 24 }
+      frame: { x: 26, y: 266, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-20',
-      frame: { x: 26, y: 246, w: 24, h: 24 }
+      frame: { x: 52, y: 266, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-21',
-      frame: { x: 52, y: 246, w: 24, h: 24 }
+      frame: { x: 78, y: 266, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-22',
-      frame: { x: 62, y: 118, w: 24, h: 24 }
+      frame: { x: 104, y: 266, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-23',
-      frame: { x: 88, y: 118, w: 24, h: 24 }
+      frame: { x: 130, y: 266, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-24',
-      frame: { x: 114, y: 118, w: 24, h: 24 }
+      frame: { x: 156, y: 266, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-25',
-      frame: { x: 140, y: 118, w: 24, h: 24 }
+      frame: { x: 36, y: 138, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-26',
-      frame: { x: 174, y: 0, w: 24, h: 24 }
+      frame: { x: 62, y: 138, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-27',
-      frame: { x: 174, y: 26, w: 24, h: 24 }
+      frame: { x: 88, y: 138, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-28',
-      frame: { x: 174, y: 52, w: 24, h: 24 }
+      frame: { x: 114, y: 138, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-29',
-      frame: { x: 174, y: 78, w: 24, h: 24 }
+      frame: { x: 140, y: 138, w: 24, h: 24 }
     },
     {
       filename: 'actor-heart-30',
-      frame: { x: 174, y: 104, w: 24, h: 24 }
+      frame: { x: 166, y: 138, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-00',
-      frame: { x: 174, y: 130, w: 24, h: 24 }
+      frame: { x: 192, y: 138, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-01',
-      frame: { x: 0, y: 168, w: 24, h: 24 }
+      frame: { x: 0, y: 188, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-02',
-      frame: { x: 26, y: 168, w: 24, h: 24 }
+      frame: { x: 26, y: 188, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-03',
-      frame: { x: 52, y: 168, w: 24, h: 24 }
+      frame: { x: 52, y: 188, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-04',
-      frame: { x: 78, y: 168, w: 24, h: 24 }
+      frame: { x: 78, y: 188, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-05',
-      frame: { x: 104, y: 168, w: 24, h: 24 }
+      frame: { x: 104, y: 188, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-06',
-      frame: { x: 130, y: 168, w: 24, h: 24 }
+      frame: { x: 130, y: 188, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-07',
-      frame: { x: 156, y: 168, w: 24, h: 24 }
+      frame: { x: 156, y: 188, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-08',
-      frame: { x: 200, y: 0, w: 24, h: 24 }
+      frame: { x: 182, y: 188, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-09',
-      frame: { x: 200, y: 26, w: 24, h: 24 }
+      frame: { x: 208, y: 188, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-10',
-      frame: { x: 200, y: 52, w: 24, h: 24 }
+      frame: { x: 0, y: 214, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-11',
-      frame: { x: 200, y: 78, w: 24, h: 24 }
+      frame: { x: 26, y: 214, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-12',
-      frame: { x: 200, y: 104, w: 24, h: 24 }
+      frame: { x: 52, y: 214, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-13',
-      frame: { x: 200, y: 130, w: 24, h: 24 }
+      frame: { x: 78, y: 214, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-14',
-      frame: { x: 200, y: 156, w: 24, h: 24 }
+      frame: { x: 104, y: 214, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-15',
-      frame: { x: 0, y: 194, w: 24, h: 24 }
+      frame: { x: 130, y: 214, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-16',
-      frame: { x: 26, y: 194, w: 24, h: 24 }
+      frame: { x: 156, y: 214, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-17',
-      frame: { x: 52, y: 194, w: 24, h: 24 }
+      frame: { x: 182, y: 214, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-18',
-      frame: { x: 36, y: 118, w: 24, h: 24 }
+      frame: { x: 204, y: 54, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-19',
-      frame: { x: 104, y: 194, w: 24, h: 24 }
+      frame: { x: 242, y: 0, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-20',
-      frame: { x: 130, y: 194, w: 24, h: 24 }
+      frame: { x: 242, y: 26, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-21',
-      frame: { x: 156, y: 194, w: 24, h: 24 }
+      frame: { x: 242, y: 52, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-22',
-      frame: { x: 182, y: 194, w: 24, h: 24 }
+      frame: { x: 242, y: 78, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-23',
-      frame: { x: 226, y: 0, w: 24, h: 24 }
+      frame: { x: 242, y: 104, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-24',
-      frame: { x: 226, y: 26, w: 24, h: 24 }
+      frame: { x: 242, y: 130, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-25',
-      frame: { x: 226, y: 52, w: 24, h: 24 }
+      frame: { x: 242, y: 156, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-26',
-      frame: { x: 226, y: 78, w: 24, h: 24 }
+      frame: { x: 242, y: 182, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-27',
-      frame: { x: 226, y: 104, w: 24, h: 24 }
+      frame: { x: 242, y: 208, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-28',
-      frame: { x: 226, y: 130, w: 24, h: 24 }
+      frame: { x: 0, y: 240, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-29',
-      frame: { x: 226, y: 156, w: 24, h: 24 }
+      frame: { x: 26, y: 240, w: 24, h: 24 }
     },
     {
       filename: 'actor-star-30',
-      frame: { x: 226, y: 182, w: 24, h: 24 }
+      frame: { x: 52, y: 240, w: 24, h: 24 }
     },
     {
       filename: 'button-both-off',
-      frame: { x: 120, y: 100, w: 16, h: 16 }
+      frame: { x: 156, y: 164, w: 16, h: 16 }
     },
     {
       filename: 'button-both-on',
-      frame: { x: 138, y: 100, w: 16, h: 16 }
+      frame: { x: 138, y: 164, w: 16, h: 16 }
     },
     {
       filename: 'button-heart-off',
-      frame: { x: 208, y: 194, w: 16, h: 16 }
+      frame: { x: 218, y: 138, w: 16, h: 16 }
     },
     {
       filename: 'button-heart-on',
-      frame: { x: 156, y: 144, w: 16, h: 16 }
+      frame: { x: 200, y: 266, w: 16, h: 16 }
     },
     {
       filename: 'button-star-off',
-      frame: { x: 138, y: 144, w: 16, h: 16 }
+      frame: { x: 174, y: 164, w: 16, h: 16 }
     },
     {
       filename: 'button-star-on',
-      frame: { x: 102, y: 50, w: 16, h: 16 }
+      frame: { x: 210, y: 164, w: 16, h: 16 }
     },
     {
       filename: 'gate-both-1',
-      frame: { x: 0, y: 68, w: 16, h: 48 }
+      frame: { x: 0, y: 138, w: 16, h: 48 }
     },
     {
       filename: 'gate-both-2',
-      frame: { x: 18, y: 68, w: 16, h: 48 }
+      frame: { x: 18, y: 138, w: 16, h: 48 }
     },
     {
       filename: 'gate-both-3',
-      frame: { x: 36, y: 68, w: 16, h: 48 }
+      frame: { x: 0, y: 88, w: 16, h: 48 }
     },
     {
       filename: 'gate-both-4',
-      frame: { x: 54, y: 68, w: 16, h: 48 }
+      frame: { x: 18, y: 88, w: 16, h: 48 }
     },
     {
       filename: 'gate-both-5',
-      frame: { x: 102, y: 0, w: 16, h: 48 }
+      frame: { x: 36, y: 88, w: 16, h: 48 }
     },
     {
       filename: 'gate-heart-1',
-      frame: { x: 90, y: 68, w: 16, h: 48 }
+      frame: { x: 54, y: 88, w: 16, h: 48 }
     },
     {
       filename: 'gate-heart-2',
-      frame: { x: 120, y: 0, w: 16, h: 48 }
+      frame: { x: 72, y: 88, w: 16, h: 48 }
     },
     {
       filename: 'gate-heart-3',
-      frame: { x: 120, y: 50, w: 16, h: 48 }
+      frame: { x: 90, y: 88, w: 16, h: 48 }
     },
     {
       filename: 'gate-heart-4',
-      frame: { x: 138, y: 0, w: 16, h: 48 }
+      frame: { x: 108, y: 88, w: 16, h: 48 }
     },
     {
       filename: 'gate-heart-5',
-      frame: { x: 138, y: 50, w: 16, h: 48 }
+      frame: { x: 126, y: 88, w: 16, h: 48 }
     },
     {
       filename: 'gate-star-1',
-      frame: { x: 156, y: 0, w: 16, h: 48 }
+      frame: { x: 144, y: 88, w: 16, h: 48 }
     },
     {
       filename: 'gate-star-2',
-      frame: { x: 156, y: 50, w: 16, h: 48 }
+      frame: { x: 162, y: 88, w: 16, h: 48 }
     },
     {
       filename: 'gate-star-3',
-      frame: { x: 0, y: 118, w: 16, h: 48 }
+      frame: { x: 180, y: 88, w: 16, h: 48 }
     },
     {
       filename: 'gate-star-4',
-      frame: { x: 18, y: 118, w: 16, h: 48 }
+      frame: { x: 198, y: 88, w: 16, h: 48 }
     },
     {
       filename: 'gate-star-5',
-      frame: { x: 72, y: 68, w: 16, h: 48 }
+      frame: { x: 216, y: 88, w: 16, h: 48 }
     },
     {
       filename: 'goal-1',
-      frame: { x: 68, y: 0, w: 32, h: 32 }
+      frame: { x: 136, y: 54, w: 32, h: 32 }
     },
     {
       filename: 'goal-2',
-      frame: { x: 34, y: 34, w: 32, h: 32 }
+      frame: { x: 170, y: 54, w: 32, h: 32 }
     },
     {
       filename: 'goal-3',
-      frame: { x: 0, y: 34, w: 32, h: 32 }
+      frame: { x: 0, y: 54, w: 32, h: 32 }
     },
     {
       filename: 'goal-4',
-      frame: { x: 34, y: 0, w: 32, h: 32 }
+      frame: { x: 102, y: 54, w: 32, h: 32 }
     },
     {
       filename: 'goal-5',
-      frame: { x: 0, y: 0, w: 32, h: 32 }
+      frame: { x: 68, y: 54, w: 32, h: 32 }
     },
     {
       filename: 'goal-6',
-      frame: { x: 68, y: 34, w: 32, h: 32 }
+      frame: { x: 34, y: 54, w: 32, h: 32 }
     },
     {
       filename: 'spike-both',
-      frame: { x: 78, y: 246, w: 16, h: 16 }
+      frame: { x: 0, y: 18, w: 240, h: 16 }
     },
     {
       filename: 'spike-heart',
-      frame: { x: 96, y: 246, w: 16, h: 16 }
+      frame: { x: 0, y: 0, w: 240, h: 16 }
     },
     {
       filename: 'spike-star',
-      frame: { x: 114, y: 246, w: 16, h: 16 }
+      frame: { x: 0, y: 36, w: 240, h: 16 }
     },
     {
       filename: 'stable-platform-both-medium',
-      frame: { x: 36, y: 144, w: 32, h: 16 }
+      frame: { x: 36, y: 164, w: 32, h: 16 }
     },
     {
       filename: 'stable-platform-both-short',
-      frame: { x: 156, y: 100, w: 16, h: 16 }
+      frame: { x: 192, y: 164, w: 16, h: 16 }
     },
     {
       filename: 'stable-platform-heart-medium',
-      frame: { x: 70, y: 144, w: 32, h: 16 }
+      frame: { x: 70, y: 164, w: 32, h: 16 }
     },
     {
       filename: 'stable-platform-heart-short',
-      frame: { x: 182, y: 168, w: 16, h: 16 }
+      frame: { x: 182, y: 266, w: 16, h: 16 }
     },
     {
       filename: 'stable-platform-star-medium',
-      frame: { x: 104, y: 144, w: 32, h: 16 }
+      frame: { x: 104, y: 164, w: 32, h: 16 }
     },
     {
       filename: 'stable-platform-star-short',
-      frame: { x: 234, y: 220, w: 16, h: 16 }
+      frame: { x: 218, y: 266, w: 16, h: 16 }
     },
   ]
 };
--- src/app/objects/spikes.js
+++ src/app/objects/spikes.js
@@ -4,18 +4,29 @@ import { enableBody } from '../components/arcadePhysics';
 export default function spikes (g, parent, owner) {
   const container = parent.add(g.add.group());
 
-  const createSprite = () => enableBody(
-    container.create(0, 0, 'sprites', `spike-${owner}`),
-    (b) => {
-      b.setSize(b.width - 2, 8, 1, 8);
-      b.immovable = true;
-    });
+  const createSprite = () => {
+    const sprite = enableBody(
+      container.create(0, 0, 'sprites', `spike-${owner}`),
+      (b) => {
+        b.setSize(14, 8, 1, 8);
+        b.immovable = true;
+      });
+    sprite.crop(new Phaser.Rectangle(0, 0, 16, 16));
+    return sprite;
+  };
   const getSpike = () => container.getFirstDead() || createSprite();
+
+  function setWidth (o, width) {
+    o.body.setSize(width - 2, 8, 1, 8);
+    o.cropRect.width = width;
+    o.updateCrop();
+    return o;
+  }
 
   return {
     setup (spikes) {
       container.callAll('kill');
-      spikes.forEach((o) => getSpike().reset(o.x, o.y));
+      spikes.forEach((o) => setWidth(getSpike(), o.width).reset(o.x, o.y));
     },
 
     collide (actor) {
